5cf604f36cb7aeb4b20f27f14ba23ddc6bb105e6
C <earthhominid@gmail.com>
2023-02-06 16:47:50 -0300

    production build

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const path = require('path');
 const dotenv = require('dotenv').config();
 const colors = require('colors');
 const connectMongoDatabase = require('./configDB/db');
@@ -14,6 +15,19 @@ app.use(express.urlencoded({ extended: false }));
 app.use('/api/blogs', require('./routes/blogRoutes'));
 app.use('/api/users', require('./routes/userRoutes'));
 app.use(errorHandler);
+
+// Server frontend
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../frontend/build')));
+
+  app.get('*', (req, res) =>
+    res.sendFile(
+      path.resolve(__dirname, '../', 'frontend', 'build', 'index.html')
+    )
+  );
+} else {
+  app.get('/', (req, res) => res.send('Please set to production'));
+}
 
 app.listen(port, () => {
   console.log(`Server started on ${port}`);
